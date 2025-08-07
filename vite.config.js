810e061412288fc2ab3909108e426adb46e31d5d
Rob Slavin <rob@flarehr.com>
2025-08-07 11:51:16 +1000

    github docs tweak

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/morning-routine-tracker/",
   root: "./src",
   build: {
-    outDir: "../dist",
+    outDir: "../docs",
     emptyOutDir: true,
   },
   server: {
